fd8a580b5f963f600263efd097feaef6f0bad92e
黄海权 <haiquan.huang411@gmail.com>
2022-08-31 00:01:46 +0800

    ✨ feat: 添加一些例子

--- main.go
+++ main.go
@@ -1,12 +1,62 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/gorilla/src/calc"
 	"github.com/gorilla/src/gee"
+	"rsc.io/quote"
 )
 
+type Person interface {
+	getName() string
+}
+
+type Student struct {
+	name string
+	age  int
+}
+
+// func (stu *Student) getName() string {
+// 	return stu.name
+// }
+
+type Worker struct {
+	name   string
+	gender string
+}
+
+func (w *Worker) getName() string {
+	return w.name
+}
+
+func ex(p Person) {
+	fmt.Println(p.getName())
+}
+func ex2(s *Student) {
+	fmt.Println(s.name)
+}
+
 func main() {
+	//接口测试
+	var _ Person = (*Worker)(nil)
+	stu := &Student{
+		name: "小明",
+		age: 14,
+	}
+	ex2(stu)
+	worker := &Worker{
+		name: "小李",
+		gender: "男",
+	}
+	//调用不同包路径下的函数
+	fmt.Printf("calc.Add(1, 3): %v\n", calc.Add(1, 3))
+	//调用第三方包
+	fmt.Printf("quote.Hello(): %v\n", quote.Hello())
+	ex(worker)
+
+	//启动服务
 	r := gee.New()
 	r.GET("/", func(c *gee.Context) {
 		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
