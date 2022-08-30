b1af22979e9e07ae25f69a51e41d0494ce1f3733
黄海权 <haiquan.huang411@gmail.com>
2022-08-30 22:56:31 +0800

    ✨ feat: day2

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gorilla/src/gee"
@@ -9,43 +8,20 @@ import (
 
 func main() {
 	r := gee.New()
-
-	r.GET("/",func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w,"URL.Path = %q\n",r.URL.Path)
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+	r.GET("/hello", func(c *gee.Context) {
+		// expect /hello?name=geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
 
-	go r.GET("/getUser",func(w http.ResponseWriter, r *http.Request) {
-		for k, v := range r.Header {
-			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-		}
+	r.POST("/login", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
 	})
-	r.Run(":9998")
+
+	r.Run(":9999")
 }
-
-// func sendMsg(ch chan int) {
-// 	fmt.Println("等待 5 秒后, 往 ch 发送 10 <- 10")
-// 	time.Sleep(time.Second * 5)
-//     ch <- 10
-// }
-
-// func recv(ch chan int){
-// 	v := <-ch
-// 	fmt.Printf("v = %v\n",v)
-// }
-
-// func main() {
-// 	//定义一个通道 ch
-//     ch := make(chan int)
-// 	ch2 := make(chan int)
-
-// 	//暂停 3 秒
-// 	// 开启异步 go 关键字 是把这个函数变为异步执行
-//     go sendMsg(ch)
-// 	fmt.Println("开始监听 ch")
-// 	go recv(ch)
-// 	// fmt.Println("开始监听 ch")
-// 	for i := range ch2 {
-// 		fmt.Printf("收到:%v\n",i)
-// 	}
-
-// }
--- src/gee/gee.go
+++ src/gee/gee.go
@@ -1,45 +1,40 @@
 package gee
 
-import (
-	"fmt"
-	"net/http"
-)
+import "net/http"
 
-//定义一个函数类型
-type HandlerFunc func (http.ResponseWriter,*http.Request) 
+// HandlerFunc defines the request handler used by gee
+type HandlerFunc func(*Context)
 
-//顶一个类型 Engine 
-//属性 router 是一个 map key 为 string value 是 一个函数类型
-type Engine struct{
-	router map[string]HandlerFunc
+// Engine implement the interface of ServeHTTP
+type Engine struct {
+	router *router
 }
 
-func New() *Engine{
-	return &Engine{router: make(map[string]HandlerFunc)}
+// New is the constructor of gee.Engine
+func New() *Engine {
+	return &Engine{router: newRouter()}
 }
 
-func (engine *Engine) addRoute(method string,pattern string,handlerFunc HandlerFunc){
-	key := method + "-" +pattern
-	engine.router[key] = handlerFunc
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	engine.router.addRoute(method, pattern, handler)
 }
 
-func (engine *Engine) GET(palette string,handlerFunc HandlerFunc){	
-	engine.addRoute("GET",palette,handlerFunc)
+// GET defines the method to add GET request
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
 }
 
-func (engine *Engine) POST(palette string,handlerFunc HandlerFunc){
-	engine.addRoute("POST",palette,handlerFunc)
+// POST defines the method to add POST request
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
 }
 
+// Run defines the method to start a http server
 func (engine *Engine) Run(addr string) (err error) {
 	return http.ListenAndServe(addr, engine)
 }
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + "-" + req.URL.Path
-	if handler, ok := engine.router[key]; ok {
-		handler(w, req)
-	} else {
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
-	}
+	c := newContext(w, req)
+	engine.router.handle(c)
 }
