d5bd2c38a5022cbac0966d42d480cd2cde8fdb70
黄海权 <haiquan.huang411@gmail.com>
2022-08-31 10:39:33 +0800

    🐞 fix: 使用端口

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/src/gee"
 )
 
 type Person interface {
@@ -38,13 +42,16 @@ func ex2(s *Student) {
 
 func main() {
 	var (
-        name    string
+        flag    string
+		port int
     )
 	fmt.Print("是否启动 (y/n)：")
-    fmt.Scan(&name)
-	if name != "y" {
+    fmt.Scan(&flag)
+	if flag != "y" {
 		return
 	}
+	fmt.Print("请输入端口号:")
+	fmt.Scan(&port)
 	//接口测试
 	// var _ Person = (*Worker)(nil)
 	// stu := &Student{
@@ -52,32 +59,32 @@ func main() {
 	// 	age: 14,
 	// }
 	// ex2(stu)
-	worker := &Worker{
-		name: "小李",
-		gender: "男",
-	}
-	//调用不同包路径下的函数
-	// fmt.Printf("calc.Add(1, 3): %v\n", calc.Add(1, 3))
-	//调用第三方包
-	// fmt.Printf("quote.Hello(): %v\n", quote.Hello())
-	ex(worker)
+	// worker := &Worker{
+	// 	name: "小李",
+	// 	gender: "男",
+	// }
+	// //调用不同包路径下的函数
+	// // fmt.Printf("calc.Add(1, 3): %v\n", calc.Add(1, 3))
+	// //调用第三方包
+	// // fmt.Printf("quote.Hello(): %v\n", quote.Hello())
+	// ex(worker)
 
 	//
-	// r := gee.New()
-	// r.GET("/", func(c *gee.Context) {
-	// 	c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
-	// })
-	// r.GET("/hello", func(c *gee.Context) {
-	// 	// expect /hello?name=geektutu
-	// 	c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	// })
+	r := gee.New()
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+	r.GET("/hello", func(c *gee.Context) {
+		// expect /hello?name=geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
 
-	// r.POST("/login", func(c *gee.Context) {
-	// 	c.JSON(http.StatusOK, gee.H{
-	// 		"username": c.PostForm("username"),
-	// 		"password": c.PostForm("password"),
-	// 	})
-	// })
-
-	// r.Run(":9999")
+	r.POST("/login", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
+	})
+	println("启动项目 监听 %v 端口",port)
+	r.Run(":"+ strconv.Itoa(port))
 }
